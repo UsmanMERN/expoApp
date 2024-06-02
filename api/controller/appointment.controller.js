130cfef61fee86d56a8f481a47db8d71b95ca3d2
unknown <saummanzoor49@gmail.com>
2024-06-02 18:03:36 +0500

    get appointments by patientId and doctorName

--- api/controller/appointment.controller.js
+++ api/controller/appointment.controller.js
@@ -20,66 +20,36 @@ exports.addAppointment = async (req, res) => {
   }
 };
 
-// Get all users
-exports.getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error fetching users", error: error.message });
-  }
-};
-
-// Get a specific user by ID
-exports.getUserById = async (req, res) => {
-  try {
-    const { email } = req.params;
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+exports.getAppontmentsByPatientId = async (req, res) => {
+    try {
+      // Extract user data from the request body
+      const {patientId } = req.body;
+  
+      // Create a new user document
+      const appointments = await Appointment.find({patientId})
+  
+  
+      res.status(201).json({ message: "Appointment Fetched",data:appointments });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Appointment Fetched patient", error: error.message });
     }
-    res.status(200).json(user);
-  } catch (error) {
-    console.error("Error fetching user data:", error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-};
+  };
 
-// Update a user by ID
-exports.updateUser = async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const updatedData = req.body;
-    const user = await User.findByIdAndUpdate(userId, updatedData, {
-      new: true,
-    });
-    if (!user) {
-      res.status(404).json({ message: "User not found" });
-    } else {
-      res.status(200).json(user);
+  exports.getAppontmentsByDoctorId = async (req, res) => {
+    try {
+      const {doctorName } = req.body;
+  
+      const appointments = await Appointment.find({doctorName})
+  
+  
+      res.status(201).json({ message: "Appointment Fetched",data:appointments });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Appointment Fetched doctor", error: error.message });
     }
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error updating user", error: error.message });
-  }
-};
-
-// Delete a user by ID
-exports.deleteUser = async (req, res) => {
-  try {
-    const userId = req.params.id;
-    const user = await User.findByIdAndRemove(userId);
-    if (!user) {
-      res.status(404).json({ message: "User not found" });
-    } else {
-      res.status(200).json({ message: "User deleted successfully" });
-    }
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: "Error deleting user", error: error.message });
-  }
-};
+  };
+  
+  
